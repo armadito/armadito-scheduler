1e6ee0e4c9b70dca7c7391fdff1a203268ec6f87
Valentin Hamon <vhamon@teclib.com>
2016-10-06 10:45:09 +0200

    Add init method for scheduler

--- lib/Armadito/Scheduler.pm
+++ lib/Armadito/Scheduler.pm
@@ -3,27 +3,51 @@ package Armadito::Scheduler;
 use 5.008000;
 use strict;
 use warnings;
+use English qw(-no_match_vars);
 
 require Exporter;
+
+use Armadito::Scheduler::Config;
+use Armadito::Scheduler::Logger qw (LOG_DEBUG LOG_INFO LOG_DEBUG2);
 
 our $VERSION = '0.0.2_01';
 
 sub new {
-	my ( $class, %params ) = @_;
+    my ( $class, %params ) = @_;
 
-	my $self = {
-		status  => 'unknown',
-		confdir => $params{confdir},
-		datadir => $params{datadir},
-		libdir  => $params{libdir},
-		vardir  => $params{vardir},
-		sigterm => $params{sigterm},
-		targets => [],
-		tasks   => []
-	};
-	bless $self, $class;
+    my $self = {
+        status  => 'unknown',
+        confdir => $params{confdir},
+        datadir => $params{datadir},
+        libdir  => $params{libdir},
+        vardir  => $params{vardir},
+        sigterm => $params{sigterm},
+        targets => [],
+        tasks   => []
+    };
+    bless $self, $class;
 
-	return $self;
+    return $self;
+}
+
+sub init {
+    my ( $self, %params ) = @_;
+
+    $self->{config} = Armadito::Scheduler::Config->new(
+        confdir => $self->{confdir},
+        options => $params{options}
+    );
+
+    my $verbosity =
+        $self->{config}->{debug} && $self->{config}->{debug} == 1 ? LOG_DEBUG
+      : $self->{config}->{debug} && $self->{config}->{debug} == 2 ? LOG_DEBUG2
+      :                                                             LOG_INFO;
+
+    $self->{logger} = Armadito::Scheduler::Logger->new(
+        config    => $self->{config},
+        backends  => $self->{config}->{logger},
+        verbosity => $verbosity
+    );
 }
 
 1;
