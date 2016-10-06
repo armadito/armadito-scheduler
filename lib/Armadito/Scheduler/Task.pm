9713a9255762589a0654dbe4167c63486e81a4d9
Valentin Hamon <vhamon@teclib.com>
2016-10-06 09:49:49 +0200

    Add base Task class

---
+++ lib/Armadito/Scheduler/Task.pm
@@ -0,0 +1,45 @@
+package Armadito::Scheduler::Task;
+
+use strict;
+use warnings;
+
+sub new {
+	my ( $class, %params ) = @_;
+
+	my $self = {
+		logger => $params{logger} || Armadito::Scheduler::Logger->new(),
+		config => $params{config},
+		agent  => $params{agent}
+	};
+
+	bless $self, $class;
+	return $self;
+}
+
+sub run {
+	my ( $self, %params ) = @_;
+
+	return $self;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Armadito::Scheduler::Task - Armadito Scheduler Task base class.
+
+=head1 DESCRIPTION
+
+This is a base class for each scheduled Tasks
+
+=head1 FUNCTIONS
+
+=head2 run ( $self, %params )
+
+Run the task.
+
+=head2 new ( $self, %params )
+
+Instanciate Task. Set task's default logger.
